014bf1815bb005dd3ddc5483801412e6b27a89c5
Max <91070715+MaxPanamax@users.noreply.github.com>
2023-02-21 21:37:40 +0200

    Xamarin_Android
    Xamarin_Android

---
+++ Android_Lis_2/Android_Lis_2/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Android_Lis_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            checkBox1.IsChecked=true;
+        }
+        //protected override void OnAppearing()
+        //{
+        //   StackLayout layout=new StackLayout();
+        //    Label label=new Label();
+        //    label.Text = "Урок 4";
+        //    label.TextColor = Color.Cyan;
+        //    label.FontSize = 20;
+        //    label.FontAttributes=FontAttributes.Bold;
+        //    label.HorizontalOptions = LayoutOptions.Center;  
+
+        //    Entry input=new Entry();
+        //    input.Placeholder = "Пароль";
+        //    input.IsPassword=true;
+        //    Entry input1 = new Entry()
+        //    {
+        //        IsEnabled = false,
+        //        Text = "*Макс что-то напсал*"
+        //    };
+        //    Label sub = new Label() { Text = "Подписатся", FontSize = 25, FontAttributes = FontAttributes.Bold };
+        //    StackLayout horizontalStack = new StackLayout();
+        //    horizontalStack.Orientation = StackOrientation.Horizontal;
+        //    CheckBox checkBox = new CheckBox() { IsChecked = true };
+
+        //    horizontalStack.Children.Add(sub);
+        //    horizontalStack.Children.Add(checkBox);
+
+        //    layout.Children.Add(label);
+        //    layout.Children.Add(input);
+        //    layout.Children.Add(input1);
+        //    layout.Children.Add(horizontalStack);
+        //    Content = layout;
+        //}
+    }
+}
---
+++ Xamarin_HW_8/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_HW_8
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            Content = new StackLayout()
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children =
+                {
+                    //new Label { Text = "Основной цвет",Margin=new Thickness(10,10,10,10) },
+                    new BoxView { Color = Color.Blue },
+                    new BoxView { Color = Color.Red },
+                    new BoxView { Color = Color.Yellow },
+
+                    //new Label { Text = "Смешанные цвета",Margin=new Thickness(0, 30, 0, 10) },
+                    new BoxView { Color = Color.Orange },
+                    new BoxView { Color = Color.Purple },
+                    new BoxView { Color = Color.Green }
+                },
+                Spacing = 15
+            };
+        }
+    }
+}
