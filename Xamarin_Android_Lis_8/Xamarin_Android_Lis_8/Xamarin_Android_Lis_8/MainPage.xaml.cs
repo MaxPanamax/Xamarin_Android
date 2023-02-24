ac43bd6d96a0353ba95a23043e3bc7127140605e
Max <91070715+MaxPanamax@users.noreply.github.com>
2023-02-24 16:53:27 +0200

    Xamarin_Android
    Xamarin_Android

---
+++ Xamarin_Android_Lis_8/Xamarin_Android_Lis_8/Xamarin_Android_Lis_8/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_Android_Lis_8
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        //protected override void OnAppearing()
+        //{
+        //    Content = new StackLayout()
+        //    {
+        //        Margin = new Thickness(15),
+        //        Orientation=StackOrientation.Horizontal,
+        //        Children =
+        //        {
+        //            //new Label(){ Text="Сначало",BackgroundColor=Color.Blue,HorizontalOptions=LayoutOptions.Start},
+        //            //new Label(){Text="Середина",BackgroundColor=Color.Blue,HorizontalOptions=LayoutOptions.Center},
+        //            //new Label(){Text="Вконце",BackgroundColor=Color.Blue,HorizontalOptions=LayoutOptions.End},
+        //            //new Label(){Text="По всей ОХ",BackgroundColor=Color.Blue,HorizontalOptions=LayoutOptions.Fill}
+        //            new Label(){ Text="Сначало",BackgroundColor=Color.Blue,VerticalOptions=LayoutOptions.Start},
+        //            new Label(){Text="Середина",BackgroundColor=Color.Blue,VerticalOptions=LayoutOptions.Center},
+        //            new Label(){Text="Вконце",BackgroundColor=Color.Blue,VerticalOptions=LayoutOptions.End},
+        //            new Label(){Text="По всей ОХ",BackgroundColor=Color.Blue,VerticalOptions=LayoutOptions.Fill}
+        //        }               
+        //    };                  
+        //}                       
+    }                           
+}                               
+                                
