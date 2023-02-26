cb8c122a19bb808873f6580d2eeccd812c787052
Max <91070715+MaxPanamax@users.noreply.github.com>
2023-02-26 14:05:30 +0200

    Xamarin_Android
    Xamarin_Android

---
+++ Xamarin_android_Lis_10/Xamarin_android_Lis_10/Xamarin_android_Lis_10/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_android_Lis_10
+{
+    public partial class MainPage : ContentPage
+    {
+        StackLayout stacklayout;
+       // Frame frame;
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            BackgroundColor = Color.Pink;
+            stacklayout = new StackLayout()
+            {
+                BackgroundColor=Color.Yellow,
+                Margin=new Thickness(5,10,15,5),
+                Padding=new Thickness(15),
+                Children=
+                {
+                    new BoxView(){BackgroundColor=Color.Violet,VerticalOptions=LayoutOptions.FillAndExpand},
+                    new BoxView(){BackgroundColor=Color.LimeGreen, Margin=new Thickness(0,40,0,0),VerticalOptions=LayoutOptions.FillAndExpand},
+                }
+            };
+            Content = stacklayout;
+        }
+    }
+}
