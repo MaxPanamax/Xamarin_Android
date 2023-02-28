a413714057217111cd6cfc6cf2875d25b7029bef
Max <91070715+MaxPanamax@users.noreply.github.com>
2023-02-28 20:31:42 +0200

    Xamarin_Android
    Xamarin_Android

---
+++ Xamarin_Android_Lis_13_SQL/Xamarin_Android_Lis_13_SQL/Xamarin_Android_Lis_13_SQL/Date/NoteDB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using Xamarin_Android_Lis_13_SQL.Models;
+
+namespace Xamarin_Android_Lis_13_SQL.Date
+{
+    internal class NoteDB
+    {
+        readonly SQLiteAsyncConnection _connection;
+        public NoteDB(string connectiongString)
+        {
+            _connection=new SQLiteAsyncConnection(connectiongString);
+            _connection.CreateTableAsync<Note>().Wait();
+        }
+        public Task<List<Note>>GetNotesAsync()
+        {
+            return _connection.Table<Note>().ToListAsync();
+        }
+        public Task<Note> GetNoteByIdAsync(int id)
+        {
+            return _connection.Table<Note>() .Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+        public Task<int>SaveNoteAsync(Note note)
+        {
+            if(note.Id!=0)
+            {
+                return _connection.UpdateAsync(note);
+            }
+            else
+            {
+                return _connection.InsertAsync(note);
+            }
+        }
+        public Task<int>DeleteNoteAsync(Note note)
+        {
+            return _connection.DeleteAsync(note);
+        }
+    }
+} 
---
+++ Xamarin_Android_Lis_13_SQL/Xamarin_Android_Lis_13_SQL/Xamarin_Android_Lis_13_SQL/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_Android_Lis_13_SQL
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
